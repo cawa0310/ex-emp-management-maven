8469fc096d1c3882f5c2d1283137b0b4df81bd00
cawa0310 <cawaguchi@icloud.com>
2024-11-12 15:05:43 +0900

    ex02

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -2,10 +2,11 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
@@ -16,8 +17,17 @@ public class AdministratorController {
     private AdministratorService service;
 
     @GetMapping("/toInsert")
-    public String toInsert(InsertAdministratorForm form, Model model) {
-        model.addAttribute("form", form);
+    public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+    }
+
+    @PostMapping("/insert")
+    public String inset(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+        service.insert(administrator);
+        return "redirect:/";
     }
 }
